2e5db259f4e853b2af6121925234a9b0774fc6ba
Ivan <iv.medvedev179@gmail.com>
2020-11-22 21:50:52 +0300

    text linter added

--- doctor.rkt
+++ doctor.rkt
@@ -1,72 +1,82 @@
-; заготовка "Доктора". Август 2019
 #lang scheme/base
 ; В учебных целях используется базовая версия Scheme
 
 ; основная функция, запускающая "Доктора"
 ; параметр name -- имя пациента
-; убрать флаг из параметров
+
+(require racket/string)
+(require racket/format)
+(require racket/set)
+
+; множество знаков пунктуации
+(define punct (set "," ";" ":"))
 
 (define (ask-patient-name)
  (begin
-  (println '(next!))
-  (println '(who are you?))
+  (printf "next!\n")
+  (printf "who are you?\n")
   (print '**)
-  (car (read))
- ) 
-)
+  (read-line)))
 
 ;2-5 добавил стоп-слово и количество пациентов
 (define (visit-doctor stopword count)
    (let loop ((name (ask-patient-name))
               (count count))
-       (if (or (= count 0) (equal? name stopword)) (print '(time to go home))
+       (if (or (= count 0) (equal? name stopword)) (printf "time to go home\n")
            (let ()
-            (printf "(hello, ~a)\n" name)
-            (print '(what seems to be the trouble?))
+            (printf "hello, ~a\n" name)
+            (printf "what seems to be the trouble\n")
             (doctor-driver-loop name null)
             (loop (if (= count 1) name (ask-patient-name)) (- count 1))
-             )
+             ))))
+
+;spring
+(define (join-lst lst)
+  ; \\1y меняем первое вхождение на y
+  (regexp-replace* #rx" ([,;:.\\!\\?])" (string-join lst) "\\1"))
+
+;spring
+(define (lint user-response)
+  (let* ((sent (car (string-split user-response #rx"[\\.\\?!]")))
+         (letters (string-split sent ""))
+         (enchance-letters (map (lambda (x) (if (set-member? punct x) (string-append " " x) x)) letters))
+         (proc_list (string-split (string-join enchance-letters "") " "))
          )
-      )
-  )
+    proc_list))
+ 
 
 ; цикл диалога Доктора с пациентом
 ; параметр name -- имя пациента
 (define (doctor-driver-loop name history-replicas)
     (newline)
     (print '**) ; доктор ждёт ввода реплики пациента, приглашением к которому является **
-    (let ((user-response (read)))
+    (let ((user-response (read-line)))
       (cond 
-	    ((equal? user-response '(goodbye)) ; реплика '(goodbye) служит для выхода из цикла
-             (printf "(goodbye, ~a)\n" name)
-             (print '(see you next week))
-             (newline))
-            (else (print (reply user-response history-replicas)) ; иначе Доктор генерирует ответ, печатает его и продолжает цикл
-                  (doctor-driver-loop name (cons (change-person user-response) history-replicas))
-             )
-       )
-      )
-)
+	    ((equal? user-response "goodbye") ; реплика "goodbye" служит для выхода из цикла
+             (printf "goodbye, ~a\n" name)
+             (printf "see you next week\n"))
+            (else (let ((prep (lint user-response))) 
+             (printf (join-lst (reply prep history-replicas))) ; иначе Доктор генерирует ответ, печатает его и продолжает цикл
+                  (doctor-driver-loop name (cons (change-person prep) history-replicas))
+             )))))
 
 ; генерация ответной реплики по user-response -- реплике от пользователя 
 (define (reply user-response history-replicas)
   (let* ((avail-stratigies (filter (lambda (strat) ((predicate strat) user-response history-replicas)) strategies))
          (wght-strategies (map cdr avail-stratigies))
         )
-    ((weighted-random wght-strategies) user-response history-replicas)
-    )
-  )
+    ((weighted-random wght-strategies) user-response history-replicas)))
 			
 ; 1й способ генерации ответной реплики -- замена лица в реплике пользователя и приписывание к результату нового начала
 (define (qualifier-answer user-response history-replicas)
-        (append (pick-random '((you seem to think that)
-                               (you feel that)
-                               (why do you believe that)
-                               (why do you say that)
+        (cons (pick-random '("you seem to think that"
+                               "you feel that"
+                               "why do you believe that"
+                               "why do you say that"
                                ; 1-1 добавил реплики
-                               (why do you think that)
-                               (you believe that)
-                               (you think that))
+                               "why do you think that"
+                               "you believe that"
+                               "you think that")
                 )
                 (change-person user-response)
         )
@@ -79,17 +89,17 @@
 
 ; замена лица во фразе			
 (define (change-person phrase)
-        (many-replace3 '((am are)
-                        (are am)
-                        (i you)
-                        (me you)
-                        (mine yours)
-                        (my your)
-						(myself yourself)
-                        (you i)
-                        (your my)
-                        (yours mine)
-						(yourself myself))
+        (many-replace3 '(("am" "are")
+                        ("are" "am")
+                        ("i" "you")
+                        ("me" "you")
+                        ("mine" "yours")
+                        ("my" "your")
+			("myself" "yourself")
+                        ("you" "i")
+                        ("your" "my")
+                        ("yours" "mine")
+			("yourself" "myself"))
                       phrase)
  )
   
@@ -103,9 +113,7 @@
                             (many-replace replacement-pairs (cdr lst)) ; рекурсивно производятся замены в хвосте списка
                         )
                      )
-               )
-         )
-  )
+               )))
 
 
 ; 1-2 новая реализация many-replace
@@ -135,63 +143,63 @@
 
 ; 2й способ генерации ответной реплики -- случайный выбор одной из заготовленных фраз, не связанных с репликой пользователя
 (define (hedge user-response history-replicas)
-       (pick-random '((please go on)
-                       (many people have the same sorts of feelings)
-                       (many of my patients have told me the same thing)
+       (pick-random '(("please go on")
+                       ("many people have the same sorts of feelings")
+                       ("many of my patients have told me the same thing")
                        ; 1-1 добавил реплики
-                       (please continue)
-                       (it is ok)
-                       (it is normal nowadays)
+                       ("please continue")
+                       ("it is ok")
+                       ("it is normal nowadays")
                        )
          )
 )
 
 ; 3й способ генерации ответной реплики -- earlier you said + вспоминаем старую фразу
 (define (history-answer user-response history-replicas)
-  (append '(earlier you said that) (list-ref history-replicas (random (length history-replicas)))
+  (cons "earlier you said that" (list-ref history-replicas (random (length history-replicas)))
     )
   )
 
 ;2-6
 (define keywords
 '(( ; начало данных 1й группы
-    (depressed suicide exams university) ; список ключевых слов 1й группы
+    ("depressed" "suicide" "exams" "university") ; список ключевых слов 1й группы
     ( ; список шаблонов для составления ответных реплик 1й группы 
-	  (when you feel depressed, go out for ice cream)
-      (depression is a disease that can be treated)
+	  ("when you feel depressed, go out for ice cream")
+      ("depression is a disease that can be treated")
 	)
   ) ; завершение данных 1й группы
   ( ; начало данных 2й группы ...
-    (mother father parents brother sister uncle ant grandma grandpa)
+    ("mother" "father" "parents" "brother" "sister" "uncle" "ant" "grandma" "grandpa")
        (
-	  (tell me more about your * , i want to know all about your *)
-          (why do you feel that way about your * ?)
-          (are you proud of your family?)
-          (do you love your * ?)
+	  ("tell me more about your * , i want to know all about your *")
+          ("why do you feel that way about your * ?")
+          ("are you proud of your family?")
+          ("do you love your * ?")
        )
   )
   (
-    (university scheme lections)
+    ("university" "scheme" "lections")
 	(
-	  (your education is important)
-	  (how many time do you spend to learning ?)
-          (you are smart)
-          (are you studying now ?)
+	  ("your education is important")
+	  ("how many time do you spend to learning ?")
+          ("you are smart")
+          ("are you studying now ?")
 	)
   )
   ( ; 3-я группа
-    (school university club)
+    ("school" "university" "club")
        (
-         (do you have friends from * ?)
-         (do you like your * ?)
+         ("do you have friends from * ?")
+         ("do you like your * ?")
        )
   )
   ( ; 4-я группа
-    (club bar cafe restaurant)
+    ("club" "bar" "cafe" "restaurant")
        (
-          (what is your favorite dish ?)
-          (what is your favorite drink ?)
-          (what is your favorite * ?)
+          ("what is your favorite dish ?")
+          ("what is your favorite drink ?")
+          ("what is your favorite * ?")
        )
   )
 ))
@@ -268,3 +276,6 @@
                          )
   )
 (define (predicate str) (list-ref str 0))
+
+
+(visit-doctor "gg" 3)
